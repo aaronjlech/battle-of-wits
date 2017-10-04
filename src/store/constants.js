5c9ba30417dbf5ca7a566ebd6f4c9f89610537db
Aaron LeCheminant <Aaron@Brelyns-MacBook-Pro-2.local>
2017-10-04 15:39:05 -0400

    adding foundations

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,3 +1,21 @@
-export const SET_TOTAL_TEAMS = 'SET_TOTAL_TEAMS';
-export const ADD_TEAMS = 'ADD_TEAMS';
-export const SET_TOTAL_ROUNDS = 'SET_TOTAL_ROUNDS';
+
+const constants = [
+   'SET_TOTAL_TEAMS',
+   'ADD_TEAMS',
+   'SET_TOTAL_ROUNDS',
+   'SET_CURRENT_ROUND',
+   'ADD_ROUNDS',
+   'REMOVE_ROUND',
+   'SET_TEAM_SCORE',
+   'SET_CURRENT_TEAM',
+   'SET_ROUND_QUESTIONS',
+   'SET_CURRENT_QUESTION',
+   'SET_QUESTION_CLOCK',
+   'SUBMIT_ANSWER',
+   'SET_GAME_WINNER'
+].reduce( (obj, item) => {
+   obj[item] = item
+   return obj
+}, {})
+
+export default constants
